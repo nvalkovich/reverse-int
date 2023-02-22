045f177895460e7eb3379b8970f320ab8b593df0
nvalkovich <anastasiya.valkovich@mail.ru>
2023-02-23 01:00:49 +0300

    feat: add reverse-int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse (num) {
+ const positiveNum = Math.abs(num)
+ const reverted = [...positiveNum.toString()].reverse();
+ return +reverted.join('');
 }
